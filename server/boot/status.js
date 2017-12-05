e20e573aafb2b4ecd46626196221d405c3e06f83
ProgBot <ProgBot@users.noreply.github.com>
2017-12-05 14:54:48 +0000

    Change lint to AirBNB

--- server/boot/status.js
+++ server/boot/status.js
@@ -1,9 +1,7 @@
-'use strict';
-
 const fs = require('fs');
 
-module.exports = function(server) {
-  var router = server.loopback.Router();
+module.exports = function status(server) {
+  const router = server.loopback.Router();
   router.get('/lbstatus', server.loopback.status());
   // Only have the root url lead to the status page if we're not serving a client
   if (fs.existsSync('client')) {
--- server/server.js
+++ server/server.js
@@ -1,25 +1,23 @@
-'use strict';
+const fs = require('fs');
+const shell = require('shelljs');
+const chalk = require('chalk');
 
 const loopback = require('loopback');
 const boot = require('loopback-boot');
-const chalk = require('chalk');
 
 if (process.env.DL_CLIENT === 'true') {
-  const fs = require('fs');
-  const shell = require('shelljs');
-  
   // Verify git is installed and available
   if (!shell.which('git')) {
     shell.echo(chalk.red('This script requires git in order to download the latest client'));
     shell.exit(1);
   }
-  
+
   const wd = process.cwd();
   shell.cd(__dirname);
   shell.cd('..');
-  
+
   let doClone = !fs.existsSync('client');
-  
+
   // Try a pull first to cut down on setup time
   if (!doClone) {
     shell.echo(chalk.cyan('Updating client...\n') + chalk.yellow('_________________\n>>> git pull -f'));
@@ -38,11 +36,11 @@ if (process.env.DL_CLIENT === 'true') {
       shell.rm('-rf', 'client');
     }
   }
-  
+
   // (Re)download the client
   if (doClone) {
     shell.echo(chalk.cyan('Downloading client...\n') + chalk.yellow('_________________\n>>> git clone https://github.com/ProgBot/frc-scouting-app client'));
-    let success = shell.exec('git clone https://github.com/ProgBot/frc-scouting-app client').code === 0;
+    const success = shell.exec('git clone https://github.com/ProgBot/frc-scouting-app client').code === 0;
     shell.echo(chalk.yellow('_________________\n'));
     if (!success) {
       shell.echo(chalk.red('Client download failed. Aborting.'));
@@ -51,7 +49,7 @@ if (process.env.DL_CLIENT === 'true') {
     shell.echo('Client updated.');
     shell.cd('client');
   }
-  
+
   // Build the client if we aren't already up-to-date
   shell.echo(chalk.cyan('Building client...\n') + chalk.yellow('_________________\n>>> npm install && npm run build'));
   const failed = shell.exec('npm install').code !== 0 || shell.exec('npm run build').code !== 0;
@@ -60,34 +58,36 @@ if (process.env.DL_CLIENT === 'true') {
     shell.echo(chalk.red('Client build failed. Aborting.'));
     shell.exit(1);
   }
-  
+
   // Change the working directory back to what it was initially
   shell.cd(wd);
   shell.echo(chalk.green('Client update complete'));
 }
 
 
-var app = module.exports = loopback();
+const app = loopback();
+module.exports = app;
 
-app.start = function() {
+/* eslint-disable no-console */
+app.start = function start() {
   // start the web server
-  return app.listen(function() {
+  return app.listen(() => {
     app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
+    const baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      const explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
 };
+/* eslint-enable no-console */
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, (err) => {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) { app.start(); }
 });
